ec18959edbdf7811ceed1f75a90229db09f60044
mbax <matt@phozop.net>
2013-04-30 14:46:28 -0400

    1.5.2 support

---
+++ src/main/java/org/kitteh/tag/compat/v1_5_R3/DefaultHandler.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012 Matt Baxter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kitteh.tag.compat.v1_5_R3;
+
+import net.minecraft.server.v1_5_R3.Packet20NamedEntitySpawn;
+
+import org.bukkit.craftbukkit.v1_5_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.kitteh.tag.api.PacketHandler;
+import org.kitteh.tag.api.TagHandler;
+
+public class DefaultHandler extends PacketHandler {
+
+    public DefaultHandler(TagHandler handler) {
+        super(handler);
+    }
+
+    @Override
+    protected void construct() throws NoSuchFieldException, SecurityException {
+        net.minecraft.server.v1_5_R3.EntityPlayer.class.getDeclaredField("playerConnection");
+    }
+
+    @Override
+    protected Object getNetworkManager(Player player) {
+        return ((CraftPlayer) player).getHandle().playerConnection.networkManager;
+    }
+
+    @Override
+    protected String getQueueField() {
+        return "highPriorityQueue";
+    }
+
+    @Override
+    protected String getVersion() {
+        return "v1_5_R3";
+    }
+
+    @Override
+    protected void handlePacketAdd(Object o, Player owner) {
+        if (o instanceof Packet20NamedEntitySpawn) {
+            final Packet20NamedEntitySpawn packet = ((Packet20NamedEntitySpawn) o);
+            packet.b = this.handler.packet(packet.a, packet.b, owner);
+        }
+    }
+
+}
